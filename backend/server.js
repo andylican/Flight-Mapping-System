1106a9174ddd8512a4fb0935a5303d528e89909d
zhu-tom <zhutom01@gmail.com>
2020-09-20 15:49:26 -0400

    Set up basic get route

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Flight = require('./models/Flight').Flight;
+require('dotenv').config();
+
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    console.log(err ? err:'connected to mongodb');
+});
+
+app.get("/flights", (req, res) => {
+    Flight.find({}, (err, docs) => {
+        docs = docs.map(val => val.toJSON());
+        res.send(JSON.stringify(docs));
+    });
+});
+
+app.listen(8080, () => console.log('listening on :8080'));
