affb97d5ab1a09869b752547a846d0aa3870d187
Eric Xiao <42785168+mathlord2@users.noreply.github.com>
2020-09-21 21:39:45 -0400

    Added plane heading check

--- client/src/components/AircraftMap/AircraftMap.js
+++ client/src/components/AircraftMap/AircraftMap.js
@@ -33,16 +33,12 @@ export default class AircraftMap extends Component {
     }
 
     render() {
-        const planeMarkup = renderToStaticMarkup(<i className="fas fa-plane" style={{fontSize: "25px"}}/>);
-        const helicopterMarkup = renderToStaticMarkup(<i className="fas fa-helicopter" style={{fontSize: "25px"}}/>);
-        const planeIcon = divIcon({
-            html: planeMarkup,
-        });
+        const simDate = moment(this.props.date);
+
+        /*const helicopterMarkup = renderToStaticMarkup(<i className="fas fa-helicopter" style={{fontSize: "25px"}}/>);
         const helicopterIcon = divIcon({
             html: helicopterMarkup
-        });
-
-        const simDate = moment(this.props.date);
+        });*/
 
         return (
             <Map center={[45.4, -75.7]} zoom={4} zoomControl={false}>
@@ -54,13 +50,23 @@ export default class AircraftMap extends Component {
                         if (simDate.isBetween(dTime, aTime)) {
                             const minsFromDepart = Math.round(moment.duration(simDate.diff(dTime)).asMinutes());
                             let [beg, end] = leg.path.length - 1 === minsFromDepart ? [minsFromDepart-1, minsFromDepart]:[minsFromDepart, minsFromDepart + 1];
-                            const rotation = Math.round(Math.asin((leg.path[beg][0] - leg.path[end][0])/(leg.path[beg][1] - leg.path[end][1])) * (-180/Math.PI));
-                            console.log(rotation);
-                            const icon = renderToStaticMarkup(<i className="fas fa-plane" style={{fontSize: "25px", transform: `rotate(${rotation}deg)`}}/>)
-                            return <>
-                                <Polyline dashArray="4" color="black" weight={2} positions={leg.path} key={`p-${index}-${i}`}/>
-                                <Marker key={`m-${index}-${i}`} position={leg.path[minsFromDepart]} icon={divIcon({html: icon})}/>
-                            </>
+                            let rotation = Math.round(Math.asin((leg.path[end][0] - leg.path[minsFromDepart][0])/(leg.path[end][1] - leg.path[minsFromDepart][1])) * (-180/Math.PI));
+
+                            try {
+                                const lngNext = leg.path[minsFromDepart+1][1];
+                                const lngCurrent = leg.path[minsFromDepart][1];
+                                if ((lngNext < lngCurrent || (lngNext > 170 && lngCurrent < -170)) && !(lngNext < -170 && lngCurrent > 170)) { //Checks if we need to rotate plane to the left
+                                    rotation += 180;
+                                }
+
+                                const icon = renderToStaticMarkup(<i className="fas fa-plane" style={{fontSize: "25px", transform: `rotate(${rotation}deg)`}}/>);
+                                return <div key={`${index}-${i}`}>
+                                    <Polyline dashArray="4" color="black" weight={2} positions={leg.path} key={`p-${index}-${i}`}/>
+                                    <Marker key={`m-${index}-${i}`} position={leg.path[minsFromDepart]} icon={divIcon({html: icon})}/>
+                                </div>
+                            } catch(error) { //Airplane has landed
+                                console.log("Aircraft landed");
+                            }
                         }
                         return null;
                     });
